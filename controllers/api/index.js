138f0c6b9fa926a936108014b3e6613f08c13847
Traci <traci.face@gmail.com>
2021-06-04 12:33:04 -0400

    added models

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes.js');
+const gameRoutes = require('./gameRoutes.js');
+
+router.use('/users', userRoutes);
+router.use('/games', gameRoutes);
+
+module.exports = router;
