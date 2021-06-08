72fddca695e449f3ad5a306a72df6b684557bfc8
Traci <traci.face@gmail.com>
2021-06-07 22:24:38 -0400

    home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,81 +1,27 @@
-const router = require('express').Router();
-const { Games, User } = require('../models');
-const withAuth = require('../util/auth');
+const { Games } = require("../models");
 
-router.get('/', async (req, res) => {
-  try {
-    // // Get all projects and JOIN with user data
-    // const projectData = await Project.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
-
-    // // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('home', { 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
+router.get("/games", (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "appID", "playtime_forever" ],
+    include: [
+      {
+        model: Games,
+        attributes: ["id", "appID", "playtime_forever" ],
+        include: {
           model: User,
-          attributes: ['name'],
+          attributes: ["username"],
         },
-      ],
+      },
+    ],
+  })
+  // serialises the posts and then passes them into the template
+    .then((postContent) => {
+      const posts = postContent.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
-
-module.exports = router;
