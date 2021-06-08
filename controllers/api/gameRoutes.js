fb7b2c65d044285c32b6bc679513dbdf4950305b
aaron.k.keller@gmail.com <aaron.k.keller@gmail.com>
2021-06-07 22:21:48 -0400

    Added a signup page view

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,64 +1,54 @@
 const router = require("express").Router();
-const {Games, User} = require("../../models");
+const { Games, User } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // C- populate the database with list of games
-// router.post("/", (req, res) => {
-//     Post.create({
-//             title: req.body.title,
-//             content: req.body.content,
-//             user_id: req.session.user_id,
-//         })
-//         .then((postContent) => res.json(postContent))
-//         .catch((err) => {
-//             console.log(err);
-//             res.status(500)
-//                 .json(err);
-//         });
-// });
+router.post("/", (req, res) => {
+  Post.create({
+    appID: req.body.appID,
+    playtime: req.body.playtime_forever,
+    user_id: req.session.user_id,
+  })
+    .then((postContent) => res.json(postContent))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // R- Read- get a single game
 router.get("/:id", (req, res) => {
-    Games.findOne({
-            where: {
-                id: req.params.id,
-            },
-            attributes: ["id", "appID", "playtime_forever" ],
-           
-        })
-       
+  Games.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "appID", "playtime_forever"],
+  });
 });
 
 // R- Read- get all games
 router.get("/", (req, res) => {
-    console.log("======================");
-    Post.findAll({
-            attributes: ["id", "appID", "playtime_forever"],
-            order: [["playtime_forever", "DESC"]],
-           
-        })
-        .then((postContent) => res.json(postContent.reverse()))
-        .catch((err) => {
-            console.log(err);
-            res.status(500)
-                .json(err);
-        });
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "appID", "playtime_forever"],
+    order: [["playtime_forever", "DESC"]],
+  })
+    .then((postContent) => res.json(postContent.reverse()))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 // D- delete a post
-Games.deleteAll = (req, res) => {
-    Tutorial.destroy({
-      where: {},
-      truncate: false
-    })
-      .then(nums => {
-        res.send({ message: `${nums} Tutorials were deleted successfully!` });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all tutorials."
-        });
-      });
-  };
+router.delete("/del", (req, res) => {
+  Games.destroy({
+    where: {},
+  }).then(function (result) {
+    res.json({
+      status: 1,
+      data: result,
+    });
+  });
+});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,73 +1,72 @@
 const router = require("express").Router();
 const { User, Post, Comment, Games } = require("../../models");
 
-
 //C- Create- Checks user into the website
 router.post("/", (req, res) => {
-    User.create({
-      username: req.body.username,
-      password: req.body.password,
-    })
-      // stores user data
-      .then((userInfo) => {
-        req.session.save(() => {
-          req.session.user_id = userInfo.id;
-          req.session.username = userInfo.username;
-          req.session.loggedIn = true;
-  
-          res.json(userInfo);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    // stores user data
+    .then((userInfo) => {
+      req.session.save(() => {
+        req.session.user_id = userInfo.id;
+        req.session.username = userInfo.username;
+        req.session.loggedIn = true;
+
+        res.json(userInfo);
       });
-  });
-  
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 //C- Verifies and checks for user in system
 router.post("/login", (req, res) => {
-    console.log("logged in")
-    console.log(req.session)
-    User.findOne({
-      where: {
-        username: req.body.username,
-      },
-    })
-      .then((userInfo) => {
-        if (!userInfo) {
-          res.status(400).json({ message: "Username doesn't exist!" });
-          return;
-        }
-        // verify user
-        const validPassword = userInfo.checkPassword(req.body.password);
-  
-        if (!validPassword) {
-          res.status(400).json({ message: "Incorrect password!" });
-          return;
-        }
-        req.session.save(() => {
-          req.session.user_id = userInfo.id;
-          req.session.username = userInfo.username;
-          req.session.loggedIn = true;
-  
-          res.json({ user: userInfo, message: "Login complete! Welcome!" });
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+  console.log("logged in");
+  console.log(req.session);
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((userInfo) => {
+      if (!userInfo) {
+        res.status(400).json({ message: "Username doesn't exist!" });
+        return;
+      }
+      // verify user
+      const validPassword = userInfo.checkPassword(req.body.password);
 
-  router.post("/logout", (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
+      if (!validPassword) {
+        res.status(400).json({ message: "Incorrect password!" });
+        return;
+      }
+      req.session.save(() => {
+        req.session.user_id = userInfo.id;
+        req.session.username = userInfo.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: userInfo, message: "Login complete! Welcome!" });
       });
-    } else {
-      res.status(404).end();
-    }
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 // R- gets the API users
 router.get("/", (req, res) => {
@@ -88,25 +87,24 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    include: 
-    [
+    include: [
       {
         model: Games,
         attributes: ["id", "appID", "playtime_forever"],
       },
-    //   //  Comment model here:
-    //   {
-    //     model: Comment,
-    //     attributes: ["id", "commentContent", "date_created"],
-    //     include: {
-    //       model: Post,
-    //       attributes: ["title"],
-    //     },
-    //   },
-    //   {
-    //     model: Post,
-    //     attributes: ["title"],
-    //   },
+      //   //  Comment model here:
+      //   {
+      //     model: Comment,
+      //     attributes: ["id", "commentContent", "date_created"],
+      //     include: {
+      //       model: Post,
+      //       attributes: ["title"],
+      //     },
+      //   },
+      //   {
+      //     model: Post,
+      //     attributes: ["title"],
+      //   },
     ],
   })
     .then((userInfo) => {
